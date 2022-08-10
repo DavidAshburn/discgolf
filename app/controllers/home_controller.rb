a87daaec50a8bf019dd6a017da8d52397042aef0
DavidAshburn <oneinchwebbing@gmail.com>
2022-08-09 23:32:05 -0700

    styling progress

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,11 @@ class HomeController < ApplicationController
   def about
   end
   def profile
+    if current_user.courses.first != nil
+      @courses = current_user.courses
+    end
   end
   def my_courses
-    redirect_to new_course_path if current_user.courses.first == nil
     @courses = current_user.courses
   end
 end
