66529890ac0e2a2cba48c714233cb31f28101cd9
DavidAshburn <oneinchwebbing@gmail.com>
2022-08-17 16:18:29 -0700

    added cards to profile page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,17 @@ class HomeController < ApplicationController
   def profile
     if current_user.courses.first != nil
       @courses = current_user.courses
+    else
+      @courses = 0
     end
+
+    if current_user.cards.first != nil
+      @cards = current_user.cards
+      @cards = @cards.reverse()
+    else
+      @cards = 0
+    end
+
   end
 end
 
